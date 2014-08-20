42a7c3fbe93fe21b0ec8bddc1f7df2f87fc21707
Sameer Ismail <admin@samism.net>
2014-08-20 01:26:51 -0500

    miscellaneous changes

--- upload-artifact.sh
+++ upload-artifact.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-scp "./out/artifacts/percival.jar/IRC Bot (Percival).jar" root@samism.net:../var/www/html/
+scp "./out/artifacts/percival.jar/percival.jar" root@samism.net:../var/www/html/
+ssh root@samism.net 'java -jar "../var/www/html/percival.jar"'
